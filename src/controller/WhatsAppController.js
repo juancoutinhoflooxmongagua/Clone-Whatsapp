3a97934c940ede2249d68658999070b6781db23b
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-02-13 12:15:11 -0300

    fix: error in elementsPrototype

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -3,6 +3,7 @@ class WhatsAppController {
       console.log('Whatsapp Controller')
       this.elementsPrototype()
       this.loadElements()
+      this.initEvents()
   }
 
   loadElements() { 
@@ -18,26 +19,21 @@ class WhatsAppController {
         this.style.display = 'none'
     }
 
-
     Element.prototype.show = function(){
         this.style.display = 'block'
-
         return this
     }
 
-    
     Element.prototype.toggle = function(){
         this.style.display = (this.style.display === 'none') ? 'block' : 'none'
-
         return this
     }
 
-
+    // Corrigir para suportar múltiplos eventos
     Element.prototype.on = function(events, fn){
-        events.split('').forEach(event => {
+        events.split(' ').forEach(event => {
             this.addEventListener(event, fn)
         })
-
         return this
     }
 
@@ -45,31 +41,36 @@ class WhatsAppController {
         for (let name in styles){
             this.style[name] = styles[name]
         }
-
         return this
     }
 
     Element.prototype.addClass = function(name){
         this.classList.add(name)
-
         return this
     }
 
-
     Element.prototype.removeClass = function(name){
         this.classList.remove(name)
-
         return this
     }
 
     Element.prototype.toggleClass = function(name){
         this.classList.toggle(name)
-
         return this
     }
 
     Element.prototype.hasClass = function(name) {
         return this.classList.contains(name)
     }
+  }
+
+  initEvents(){
+    this.el.myPhoto.on('click', e =>{
+        this.el.panelEditProfile.addClass('open')
+    })
+    
+    this.el.btnNewContact.on('click', e =>{
+        this.el.panelAddContact.addClass('open')
+    })
   }
 }
