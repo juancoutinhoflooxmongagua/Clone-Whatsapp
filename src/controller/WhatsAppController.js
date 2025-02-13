5e736596821a963c9428d5508d4411518b7fd3bd
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-02-13 11:35:14 -0300

    feat: add format getCamelCase

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,13 @@
 class WhatsAppController{
     constructor(){
         console.log('Whatsapp Controller')
+        this.LoadElements()
+    }
+
+
+    LoadElements(){
+        document.querySelectorAll('[id]').forEach(element => {
+            this.el[Format.getCamelCase('id')] = element
+        })
     }
 }
