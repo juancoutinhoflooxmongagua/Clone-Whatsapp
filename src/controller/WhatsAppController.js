a95488f0f51ac62346375066365dd76e079ca79f
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-02-13 12:31:48 -0300

    refactor: for better code readability

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -65,33 +65,35 @@ class WhatsAppController {
   }
 
   initEvents(){
-    this.el.myPhoto.on('click', e=>{
-        this.closeAllLeftPanel();
-        this.el.panelEditProfile.show();
-        setTimeout(()=>{
-            this.el.panelEditProfile.addClass('open');
-        }, 300);
+        this.el.myPhoto.on('click', e=>{
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
         
-    });
-    this.el.btnNewContact.on('click', e=>{
-        this.closeAllLeftPanel();
-        this.el.panelAddContact.show();
-        setTimeout(()=>{
-            this.el.panelAddContact.addClass('open');
-        }, 300);
+        });
         
-    });
-    this.el.btnClosePanelEditProfile.on('click', e=>{
-        this.el.panelEditProfile.removeClass('open');
-    });
+        this.el.btnNewContact.on('click', e=>{
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+        
+        });
+        
+        this.el.btnClosePanelEditProfile.on('click', e=>{
+            this.el.panelEditProfile.removeClass('open');
+        });
    
-    this.el.btnClosePanelAddContact.on('click', e=>{
-        this.el.panelAddContact.removeClass('open');
-    });
-}
-closeAllLeftPanel(){
-    this.el.panelAddContact.hide();
-    this.el.panelEditProfile.hide();
-}
-
+        this.el.btnClosePanelAddContact.on('click', e=>{
+            this.el.panelAddContact.removeClass('open');
+        });
+    }
+    
+    closeAllLeftPanel(){
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
+    }
 }
