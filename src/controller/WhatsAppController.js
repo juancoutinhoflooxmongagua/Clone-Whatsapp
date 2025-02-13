18456208dee96626f96193107c93258378976a66
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-02-13 11:53:19 -0300

    feat: create prototype and elements for display

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,14 +1,33 @@
 class WhatsAppController {
   constructor() {
-      console.log('Whatsapp Controller');
-      this.loadElements();
+      console.log('Whatsapp Controller')
+      this.elementsPrototype()
+      this.loadElements()
   }
 
   loadElements() { 
-      this.el = {};
+      this.el = {}
 
       document.querySelectorAll("[id]").forEach((element) => {
-          this.el[Format.getCamelCase(element.id)] = element;
-      });
+          this.el[Format.getCamelCase(element.id)] = element
+      })
+  }
+
+  elementsPrototype(){
+    Element.prototype.hide = function(){
+        this.style.display = 'none'
+    }
+
+
+    Element.prototype.show = function(){
+        this.style.display = 'block'
+    }
+
+    
+    Element.prototype.toggle = function(){
+        this.style.display = (this.style.display === 'none') ? 'block' : 'none'
+    }
+
+    
   }
 }
--- src/util/format.js
+++ src/util/format.js
@@ -1,5 +1,5 @@
 class Format {
-    static getCamelCase(text) { // Corrigido para getCamelCase com "C" maiúsculo
+    static getCamelCase(text) {
         let div = document.createElement('div');
         div.innerHTML = `<div data-${text}="id"></div>`;
         return Object.keys(div.firstChild.dataset)[0];
