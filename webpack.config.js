091dea67ed15aa316b003319d8c7566d4362d3cb
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-02-21 12:21:34 -0300

    feat: fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path'); // Importação necessária
+const path = require('path'); 
 
 module.exports = {
     entry: './src/app.js',
 
-    output: { // Corrigido "outrop" para "output"
+    output: { 
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist') // Remova a barra antes de 'dist'
+        path: path.resolve(__dirname, 'dist')
     }
 };
