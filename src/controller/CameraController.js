b449552bbc6ca6558e833d80c78c1452f846befd
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-02-24 13:43:55 -0300

    feat: create method stream stop

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -5,10 +5,17 @@ export class CameraController {
         navigator.mediaDevices.getUserMedia({
             video: true
         }).then(stream => {
+
             this._videoEl.srcObject = stream;
             this._videoEl.play();
         }).catch(err => {
             console.error('Erro ao acessar a câmera: ', err);
         });
     }
-}
+        stop(){
+            stream.getTracks().forEach(track =>{
+                track.stop()
+            })
+
+        }
+    }
