585be244f3f1821cb62774da6be55c0d2ae56f71
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-02-27 10:39:53 -0300

    build: add pdf worker for weback.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,11 @@ module.exports ={
 
     entry:{
         app: './src/app.js',
+        'pdf.worker' : 'pdfjs-dist/build/pdf.worker.entry.js'
     },
     output:{
         filename:'[name].bundle.js',
         path: path.join(__dirname, 'dist'),
+        publicPath: 'dist'
     }
 }
