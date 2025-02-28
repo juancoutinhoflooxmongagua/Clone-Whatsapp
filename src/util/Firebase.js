d524ef45c915970845355d32a68018e95f2b03c7
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-02-28 13:43:05 -0300

    feat: create method for Auth with Firebase

---
+++ src/util/Firebase.js
@@ -0,0 +1,24 @@
+class Firebase {
+    init(){
+        if (!this._initializeApp(this._config))
+        firebase.firestore().settings({
+            timestampsInSnapshots: true
+        })
+
+        this.initialized = true
+    }
+
+    static db(){
+        return firebase.firestore()
+    }
+
+    static hd(){
+        return firebase.storage()
+    }
+
+    initAuth(){
+        return new Promise((s, f) => {
+            firebase.auth.signInWithPopup()
+        })
+    }
+}
