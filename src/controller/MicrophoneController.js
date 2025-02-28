0e02bed1aa1887dc96b0f16bb8afc9d4a086201c
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-02-28 11:13:15 -0300

    refactor: clean code

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -5,10 +5,8 @@ class MicrophoneController {
         navigator.mediaDevices.getUserMedia({
             audio: true
         }).then(stream => {
-            this._stream = stream;
-            
+            this._stream = stream;   
             let audio = new Audio()
-
             audio.src = URL.createObjectURL(stream)
 
             audio.play()
