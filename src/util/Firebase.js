b15602f06e35851fef5146c039a900833addac9e
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-03-05 09:48:52 -0300

    feat: add method init for firebase

--- src/util/Firebase.js
+++ src/util/Firebase.js
@@ -1,11 +1,26 @@
-class Firebase {
-    init(){
-        if (!this._initializeApp(this._config))
-        firebase.firestore().settings({
-            timestampsInSnapshots: true
-        })
+const firebase = require('Firebase')
+require('firebase/firestore')
+export class Firebase {
 
-        this.initialized = true
+    constructor(){
+        this._config = {
+          apiKey: "AIzaSyCLODBGpr_LFC-OfLgMwvVulXg-ipnvHO0",
+          authDomain: "wasabi-5d6f7.firebaseapp.com",
+          projectId: "wasabi-5d6f7",
+          storageBucket: "wasabi-5d6f7.firebasestorage.app",
+          messagingSenderId: "22057437819",
+          appId: "1:22057437819:web:7d0026bc8f2abc9105f0de"
+        }
+
+        this.init()
+    }
+
+    init(){
+
+        if (!this._initialized) {
+            firebase.initializeApp(this._config)
+            this.initialized = true
+        }
     }
 
     static db(){
